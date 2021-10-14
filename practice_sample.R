4c2d17c2f9e343dcbd95a78cf788be223dc46b45
Benjamin Levy <Benjamin.Levy@nmfs.local>
2021-10-14 13:58:20 -0400

    New stuff

--- practice_sample.R
+++ practice_sample.R
@@ -8,8 +8,8 @@
 # compare true sample mean with noise sample mean
 
 #create matrix with random values
-r <- 100
-c <- 100
+r <- 1000
+c <- 1000
 m1 <- matrix(runif(r*c), r, c)
 
 #sample the matrix n times without replacement
@@ -35,11 +35,14 @@ mean_samp_noise <- mean(samp_noise)
 
 
 # create a loop that increases the number of samples and evaluates how close they are to the true value
-x <- c(10,50,100,200,400,500,1000)
+x <- c(1:1000)
 
 idx <- 1
 
+#preallocate matrices for storage
 samp_noise <- matrix(NA,length(x),max(x))
+samp_mean <- matrix(0,1,length(x))
+mean_samp_noise <- matrix(0,1,length(x))
 
 true_mean <- mean(m1) #should be about .5
 
@@ -53,7 +56,7 @@ for (n in x){
   #create some noise same length as samp
   #then add to samp
   #rnorm(n, mean = 0, sd = 1)
-  noise <- rnorm(length(samp),0,.15)
+  noise <- rnorm(length(samp),0,.3)
   
 
     samp_noise[idx,1:n] <- samp+noise
@@ -67,9 +70,17 @@ for (n in x){
 library(tidyverse)
 library(readxl)
 library(here)
-  
-#the output is being classified as a value so must reclassify as data to plot
-mean_samp_noise <- as.data.frame(mean_samp_noise)
 
-ggplot(data = mean_samp_noise, aes(x = c(10,50,100,200,400,500,1000),y= c(10,1000)))+
-  geom_line()
+plot(x,samp_mean)
+  
+
+plot(x,mean_samp_noise)
+
+
+#the output is being classified as a value so must reclassify as data to plot
+#mean_samp_noise <- as.data.frame(mean_samp_noise)
+
+#ggplot needs to have columns/rows labeled 
+
+#ggplot(data = mean_samp_noise, aes(x = c(10,50,100,200,400,500,1000),y= c(10,1000)))+
+#  geom_line()
