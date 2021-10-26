0ca9c04c7d24e24269879a1a73f528a62ba5fac9
Benjamin Levy <Benjamin.Levy@nmfs.local>
2021-10-26 09:12:03 -0400

    Function for turning vector coordinate into (x,y) matrix coordinate

---
+++ TestScripts/pos2coord.R
@@ -0,0 +1,15 @@
+pos2coord<-function(pos=NULL, coord=NULL, dim.mat=NULL){
+  if(is.null(pos) & is.null(coord) | is.null(dim.mat)){
+    stop("must supply either 'pos' or 'coord', and 'dim.mat'")
+  }
+  if(is.null(pos) & !is.null(coord) & !is.null(dim.mat)){
+    pos <- ((coord[,2]-1)*dim.mat[1])+coord[,1] 
+    return(pos)
+  }
+  if(!is.null(pos) & is.null(coord) & !is.null(dim.mat)){
+    coord <- matrix(NA, nrow=length(pos), ncol=2)
+    coord[,1] <- ((pos-1) %% dim.mat[1]) +1
+    coord[,2] <- ((pos-1) %/% dim.mat[1]) +1
+    return(coord)
+  }
+}
