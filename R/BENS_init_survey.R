d1fc300bc34842053cdfe9cd400f5182a0503b58
Benjamin Levy <benjamin.levy>
2021-10-27 16:06:38 -0400

    Receives strata coordinates from hab and does stratefied random sampling in each

--- R/BENS_init_survey.R
+++ R/BENS_init_survey.R
@@ -24,7 +24,7 @@
 #' @export
 
 BENS_init_survey <- function (sim_init = NULL, design = 'fixed_station', n_stations = 50, 
-			     start_day = 90, stations_per_day = 5, Qs = NULL, strata = NULL) {
+			     start_day = 90, stations_per_day = 5, Qs = NULL, strata_coords = NULL) {
 
 	# useful indexes
 	idx       <- sim_init[["idx"]]
@@ -76,23 +76,38 @@ BENS_init_survey <- function (sim_init = NULL, design = 'fixed_station', n_stati
 	}
 
 	if(design == "random_station") {
+	  	#initialize x and y coordinate vectors
+	      x <- vector()
+	      y <- vector()
+    for(j in seq(length(strata_coords))){
 	  
-  ##Produce correct number of random x and y locations to sample from
+	  ##Produce correct number of random x and y locations to sample from
+
+	  #sample as many stations as you need. Output is the matrix positions
+	  
+      #old version before multiple stations
+      #my_sample <- sample(idx[["ncols"]]*idx[["nrows"]],n_stations*sim_init[["idx"]][["ny"]],replace = FALSE)
+      
+      row_dist <- length(strata_coords[[j]][1]:strata_coords[[j]][2]) #specifies size of given strata
+      col_dist <- length(strata_coords[[j]][3]:strata_coords[[j]][4])
+      
+      x1 <- strata_coords[[j]][1]; x2 <- strata_coords[[j]][2]
+      y1 <- strata_coords[[j]][3]; y2 <- strata_coords[[j]][4]
+      
+      show(y1)
+      #currently dividing total number of samples evening among each strata
+      my_sample <- sample(row_dist*col_dist,n_stations*sim_init[["idx"]][["ny"]]/length(strata_coords),replace = FALSE)
+      
 
 	  
-	  
-	  my_sample <- sample(idx[["ncols"]]*idx[["nrows"]],n_stations*sim_init[["idx"]][["ny"]],replace = FALSE)
+	  dim.mat = c(row_dist,col_dist) #dimension of given strata
 	  
 
-	  
-	  dim.mat = c(idx[["nrows"]],idx[["ncols"]]) #dimension of given strata
-	  
-	  x <- vector()
-	  y <- vector()
 	  coord <- matrix( NA,nrow=1,ncol=2)
 	  
+	  #translate each matrix position into an (x,y) index for given strata
 	  for(i in my_sample){
-	    source("TestScripts/pos2coord.R")
+	 
 	    
 
         pos <- i	    
@@ -101,24 +116,27 @@ BENS_init_survey <- function (sim_init = NULL, design = 'fixed_station', n_stati
 	      coord[1,1] <- ((pos-1) %% dim.mat[1]) +1  #xcoordinate
 	      coord[1,2] <- ((pos-1) %/% dim.mat[1]) +1 #y coordinate
 	   #  show(pos)
+	   	      
+#	    coords <- pos2coord(pos=i,dim.mat = dim.mat)   #I extracted the above code from this function file 
+	      
+	      
+
 	    
+	      #tack onto existing coordinates after SHIFT COORDINATES OVER INTO CORRECT STRATA
+	    x <- c(x,coord[1,1]+(x1-1))
 	    
-#	    coords <- pos2coord(pos=i,dim.mat = dim.mat)   #I extracted the above code from this function file
-	   
-	    
-	    x <- c(x,coord[1,1])
-	    
-	    y <- c(y,coord[1,2])
+	    y <- c(y,coord[1,2]+(y1-1)) #subtract 1 because index starts at 1
 	    
 	    
 	  }
+    }
 	  
 
 	##########################
 	## set up RANDOM survey log matrix
 	##########################
 	
-	#create the repeating pattern for the station days
+	#create the station days
 	station_days <- rep(sim_init[["brk.idx"]][["day.seq"]][which(sim_init[["brk.idx"]][["day.seq"]] == 92, arr.ind = T):c(which(sim_init[["brk.idx"]][["day.seq"]] == 92, arr.ind = T) + round(n_stations / stations_per_day, 0) - 1)], each = stations_per_day)[seq_len(n_stations)]
 	
 	
@@ -142,6 +160,7 @@ BENS_init_survey <- function (sim_init = NULL, design = 'fixed_station', n_stati
 	
 	if(anyDuplicated(log.mat[,2:3])!=0){"There are duplicated sampling stations"}
 
-}
+    
+	}
 
 }
