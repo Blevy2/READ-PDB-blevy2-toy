b03bedd9228ca411dd2305565cd0ebfbd7f33425
cmlegault <chris.legault@noaa.gov>
2021-12-03 16:23:56 -0500

    add second attempt at pushing from MARS

--- testpush.R
+++ testpush.R
@@ -1,3 +1,7 @@
 # testpush.R
 
 # just a test file to see if Chris can push from RStudio on MARS server
+# first attempt failed - user name and login approach no longer supported
+
+# so went to https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+# and followed directions to create personal access token
