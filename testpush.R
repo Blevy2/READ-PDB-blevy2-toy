28c5e29353ebbccc60dc16cb574f365c17d1e209
cmlegault <chris.legault@noaa.gov>
2021-12-03 16:40:52 -0500

    add yet another try at pushing from MARS

--- testpush.R
+++ testpush.R
@@ -5,5 +5,16 @@
 
 # so went to https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 # and followed directions to create personal access token
-# success!
+# success! - not so fast, PAT is not stored
+
+# so on to the next method
+# https://gist.github.com/Z3tt/3dab3535007acf108391649766409421
+## create a personal access token for authentication:
+usethis::browse_github_token() 
+
+## set personal access token:
+credentials::set_github_pat("MARS") # MARS is the name I saved the token as
+
+
+
 
