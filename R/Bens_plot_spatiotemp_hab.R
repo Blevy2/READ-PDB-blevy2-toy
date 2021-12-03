d734e883febd50eff9fc2fd5cd6b1a5a11b21ba5
Benjamin Levy <Benjamin.Levy@nmfs.local>
2021-12-03 09:39:25 -0500

    New line to plot temp and habitat preferences by month

---
+++ R/Bens_plot_spatiotemp_hab.R
@@ -0,0 +1,111 @@
+#' @title Plot spatiotemporal habitat suitability
+
+#' @description Function to plot out the habitat suitability, as adjusted by
+#' the spatiotemporal move covariates
+
+#' @param hab is the output from \link{create_hab}
+#' @param moveCov is the output from \link{init_moveCov}
+#' @param plot.file path to save the plots of the spatiotemporal habitats 
+#' @param spwn_wk is a named list of the spawning week for each population
+
+#' @examples None
+
+#' @export
+
+plot_spatiotemp_hab <- function(hab = NULL, moveCov = NULL, plot.file = NULL, spwn_wk = NULL, plot_wk = NULL, plot_monthly = NULL) {
+
+	nrows <- nrow(hab[["hab"]][[1]]) 
+	ncols <- ncol(hab[["hab"]][[1]])
+
+
+for(s in seq_len(length(hab[["hab"]]))) {
+
+	nt <- length(moveCov[["cov.matrix"]])
+	if(!is.null(plot.file)) {
+	png(filename = paste0(plot.file,'/','habitat_spatiotemp_spp_',s,'.png'), width = 800, height = 800)
+	}
+	par(mfrow = c(ceiling(sqrt(length(plot_wk))), ceiling(length(plot_wk)/ceiling(sqrt(length(plot_wk))))), mar = c(1, 1, 1, 1))
+
+	for(i in plot_wk) {
+
+	move_cov_wk <- moveCov[["cov.matrix"]][[i]]
+
+	move_cov_wk_spp <- matrix(nc = ncols,
+				 nr = nrows, 
+				 sapply(move_cov_wk, norm_fun, 
+                          mu = moveCov[["spp_tol"]][[s]][["mu"]], 
+		  	  va = moveCov[["spp_tol"]][[s]][["va"]]))
+
+		if(!i %in% spwn_wk[[s]]) {
+		image(hab[["hab"]][[paste0('spp',s)]] * move_cov_wk_spp, cex.axis = 1.5, cex.main = 2, col = grey(seq(1,0,l = 51)), axes = F)
+		}
+	
+		if(i %in% spwn_wk[[s]]) {
+		image(hab[["spwn_hab"]][[paste0('spp',s)]] * move_cov_wk_spp, cex.axis = 1.5, cex.main = 1, col = grey(seq(1,0,l = 51)), axes = F)
+		}
+	axis(1, at = seq(0, 1, by = 0.2), labels = seq(0, nrows, by = nrows/5))
+	axis(2, at = seq(0, 1, by = 0.2), labels = seq(0, ncols, by = ncols/5))
+	text(0.5, 0.98, labels = paste('week', i), cex = 1)
+
+	}
+}
+	dev.off()
+	
+	
+	
+	
+	
+	#plot a week from each month over entire simulation
+
+	if(plot_monthly == TRUE){	
+	
+	for(s in seq_len(length(hab[["hab"]]))) {
+	  
+  
+	for(k in seq(12)){
+	  
+	  	  nt <- length(moveCov[["cov.matrix"]])
+	  if(!is.null(plot.file)) {
+	    
+	    pdf(file=paste0(plot.file,'/Monthly_covariate_plots/','monthly_habitat_spatiotemp_spp_',s,'month_',k,'.pdf'))
+	    #png(filename = paste0(plot.file,'/Monthly_covariate_plots/','monthly_habitat_spatiotemp_spp_',s,'month_',k,'.png'), width = 800, height = 800)
+	  }
+	  par(mfrow = c(5,4), mar = c(1, 1, 1, 1))
+	
+	  
+	for(i in seq(1,nt,52)){
+	  
+	  month_shift <- 4*(k-1)
+	  
+	  move_cov_wk <- moveCov[["cov.matrix"]][[i+month_shift]]
+	  
+	  move_cov_wk_spp <- matrix(nc = ncols,
+	                            nr = nrows, 
+	                            sapply(move_cov_wk, norm_fun, 
+	                                   mu = moveCov[["spp_tol"]][[s]][["mu"]], 
+	                                   va = moveCov[["spp_tol"]][[s]][["va"]]))
+	  
+	  if(!i %in% spwn_wk[[s]]) {
+	    image(hab[["hab"]][[paste0('spp',s)]] * move_cov_wk_spp, cex.axis = 1.5, cex.main = 2, col = grey(seq(1,0,l = 51)), axes = F)
+	  }
+	  
+	  if(i %in% spwn_wk[[s]]) {
+	    image(hab[["spwn_hab"]][[paste0('spp',s)]] * move_cov_wk_spp, cex.axis = 1.5, cex.main = 1, col = grey(seq(1,0,l = 51)), axes = F)
+	  }
+	  axis(1, at = seq(0, 1, by = 0.2), labels = seq(0, nrows, by = nrows/5))
+	  axis(2, at = seq(0, 1, by = 0.2), labels = seq(0, ncols, by = ncols/5))
+	  text(0.5, 0.98, labels = paste('week', i), cex = 1)
+	  
+	  
+	}
+	  
+	  
+	}
+	}
+	}
+	dev.off()
+	
+
+	
+	
+}
