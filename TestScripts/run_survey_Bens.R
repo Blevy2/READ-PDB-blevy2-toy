ad2f0346596b00cdfc04c198f6db42db6818404e
Blevy2 <benjamin.levy@noaa.gov>
2021-12-15 16:54:28 -0500

    Added section to summarize multiple iteration results

--- TestScripts/run_survey_Bens.R
+++ TestScripts/run_survey_Bens.R
@@ -57,7 +57,7 @@ for(i in seq(nstrata)){
 
 
 
-#add a column to each strata_surv that will contain the month to sample from
+#add a column to each strata_surv that will contain the week to sample from
 
 
 #######################################################
@@ -133,11 +133,11 @@ for(s in seq(nstrata)){  #pull out strata
     x <- strata_surv[[s]][i,2]   #x coord in second column
     y <- strata_surv[[s]][i,3]   #y coord in third column  
     year <- strata_surv[[s]][i,7] #year in 7th column
-    month <- strata_surv[[s]][i,10]  #appended sample month into 10th column above
+    week <- strata_surv[[s]][i,10]  #appended sample week into 10th column above
 
     
-    strata_surv[[s]][i,8] <- result[[res]][["pop_bios"]][[(month+(52*(year-1)))]][["spp1"]][x,y]   #POPULATIONMATRIX$spp1(month+(52*(year-1))[x,y]   #spp1 in col 8
-    strata_surv[[s]][i,9] <- result[[res]][["pop_bios"]][[(month+(52*(year-1)))]][["spp2"]][x,y]   #POPULATIONMATRIX$spp2(month+(52*(year-1))[x,y]   #spp2 in col 9
+    strata_surv[[s]][i,8] <- result[[res]][["pop_bios"]][[(week+(52*(year-1)))]][["spp1"]][x,y]   #POPULATIONMATRIX$spp1(week+(52*(year-1))[x,y]   #spp1 in col 8
+    strata_surv[[s]][i,9] <- result[[res]][["pop_bios"]][[(week+(52*(year-1)))]][["spp2"]][x,y]   #POPULATIONMATRIX$spp2(week+(52*(year-1))[x,y]   #spp2 in col 9
     
   }
   
@@ -152,7 +152,7 @@ for(s in seq(nstrata)){  #pull out strata
   
 }
 
-survey_results <- list(list(),list(),list(),list())
+survey_results <- list("strata1","strata2","strata3","strata4")
 survey_results[[1]] <- all1
 survey_results[[2]] <- all2
 survey_results[[3]] <- all3
@@ -163,6 +163,142 @@ survey_results[[4]] <- all4
 
 
 
+
+
+##################################################################################
+#Aggregate results of all iterations into single object (mean and variance)
+##################################################################################
+
+
+### FIRST DO IT FOR THE SURVEY RESULTS
+
+#procedure
+#1) pull out given strata with all iteration results 
+#2) use Reduce to add all items in list together, then by total number in list to obtain mean
+#3) use info from https://stackoverflow.com/questions/39351013/standard-deviation-over-a-list-of-matrices-in-r to calculate standard deviation
+
+sum_survey_results <- list()
+
+
+for(i in seq(nstrata)){ #4 strata
   
+    sum_survey_results[[i]] <- Reduce("+",survey_results[[i]])/length(survey_results[[i]])
+
+    m<-survey_results[[i]] #pull out list for given strata
+    sd_mat <- matrix(apply(sapply(1:length(survey_results[[1]][[1]]), 
+                    function(x) unlist(m)[seq(x, length(unlist(m)),
+                    length(survey_results[[1]][[1]]) )]), 2, sd), 
+                    ncol = length(survey_results[[1]][[1]][1,]))
+    
+    #the sd_mat above is mostly 0 since things dont change. just pull out sample columns 8 and 9 and append them to previous
+    sum_survey_results[[i]] <- cbind(sum_survey_results[[i]],sd_mat[,8:9])
+    
+    colnames(sum_survey_results[[i]]) <- c("Matrix index","x","y","strata","day","tow","year","spp1","spp2","week","sd_spp1","sd_spp2")
+}
+
+
+
+### SECOND DO IT FOR THE POPULATION RESULTS
+
+#procedure for biomat (sum of population) and recmat (recruitment)
+#1) go through each iteration and put individual results in their own list 
+#2) use Reduce to add all items in each list together, then by total number in list to obtain mean
+#3) use info from https://stackoverflow.com/questions/39351013/standard-deviation-over-a-list-of-matrices-in-r to calculate standard deviation
+
+pop_sum_s1_biomat <- list()
+pop_sum_s1_recmat <- list()
+pop_sum_s2_biomat <- list()
+pop_sum_s2_recmat <- list()
+
+
+for(i in seq(length(result))){
   
+  pop_sum_s1_biomat[[i]] <- result[[i]][["pop_summary"]][["spp1"]][["Bio.mat"]] 
+  pop_sum_s1_recmat[[i]] <- result[[i]][["pop_summary"]][["spp1"]][["Rec.mat"]] 
+  pop_sum_s2_biomat[[i]] <- result[[i]][["pop_summary"]][["spp2"]][["Bio.mat"]]
+  pop_sum_s2_recmat[[i]] <- result[[i]][["pop_summary"]][["spp2"]][["Rec.mat"]]
   
+}
+
+sum_pop_sum_s1_biomat <- list()
+sum_pop_sum_s1_recmat <- list()
+sum_pop_sum_s2_biomat <- list()
+sum_pop_sum_s2_recmat <- list()
+
+#biomat matrices of form [year,day]
+#recmat are vector of form [recruitment in year]
+sum_pop_sum_s1_biomat <- Reduce("+", pop_sum_s1_biomat)/length(pop_sum_s1_biomat)
+sum_pop_sum_s1_recmat <- Reduce("+", pop_sum_s1_recmat)/length(pop_sum_s1_recmat)
+sum_pop_sum_s2_biomat <- Reduce("+", pop_sum_s2_biomat)/length(pop_sum_s2_biomat) 
+sum_pop_sum_s2_recmat <- Reduce("+", pop_sum_s2_recmat)/length(pop_sum_s2_recmat)
+
+
+
+#procedure for spatial population output
+#1) fix week in outer loop
+#2) go through entire result list putting each corresponding week in list
+#4) summarize that list as PopBios
+#5) go on to next week
+
+spat_pop_spp1 <- list()
+spat_pop_spp2 <- list()
+
+
+for(wk in seq(52)){ #fix week
+  
+  temp_wk_spp1 <- list()
+  temp_wk_spp2 <- list()
+  
+  for(res in seq(length(result))){ #go through results
+    
+    #make list of given week for each species
+    temp_wk_spp1[[res]] <-  result[[res]][["pop_bios"]][[wk]][["spp1"]] 
+    temp_wk_spp2[[res]] <-  result[[res]][["pop_bios"]][[wk]][["spp2"]] 
+    
+  }
+  
+  #average weekly list into single list entry
+  spat_pop_spp1[[wk]] <-  Reduce("+", temp_wk_spp1)/length(temp_wk_spp1)
+  spat_pop_spp2[[wk]] <-  Reduce("+", temp_wk_spp2)/length(temp_wk_spp2)
+  
+}
+
+
+
+##################################################################
+# PUT EVERYTHING INTO OBJECT CALLED res TO MATCH NORMAL OUTPUT SO PLOTTING FUNCTIONS MORE EASILY USED
+##################################################################
+
+
+#build spp1 and spp2 in plot summary
+spp1 <- list()
+spp2 <- list()
+spp1[["Bio.mat"]] <-sum_pop_sum_s1_biomat
+spp1[["Catch.mat"]] <- sum_pop_sum_s1_recmat
+spp2[["Bio.mat"]] <-sum_pop_sum_s2_biomat
+spp2[["Catch.mat"]] <- sum_pop_sum_s2_recmat
+
+#put spp1 and spp2 into pop_summary
+pop_summary <- list()
+pop_summary[["spp1"]] <- spp1
+pop_summary[["spp2"]] <- spp2
+
+
+
+pop_bios <- list()
+for(i in seq(52)){
+  temp <-list()
+  temp[["spp1"]] <- spat_pop_spp1[[i]]
+  temp[["spp2"]] <- spat_pop_spp2[[i]]
+  
+  pop_bios[[i]] <- temp
+  
+}
+
+
+
+#put everything into list res
+res <- list()
+
+res[["pop_summary"]] <- pop_summary
+res[["pop_bios"]] <- pop_bios
