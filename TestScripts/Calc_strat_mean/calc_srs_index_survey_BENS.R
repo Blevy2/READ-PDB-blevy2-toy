26fc2de28835201759513287bce9732419ca58d0
Blevy2 <benjamin.levy@noaa.gov>
2022-01-25 16:56:38 -0500

    stratified mean calculation

---
+++ TestScripts/Calc_strat_mean/calc_srs_index_survey_BENS.R
@@ -0,0 +1,88 @@
+# calculate design-based stratified random sampling indices
+#
+# liz brooks
+# 15 june 2020
+# added 
+# last update: august 2021
+# december 2021: added scalar for total area to get absolute index
+
+#edited by Ben Levy January 2022
+
+
+rm(list=ls(all=TRUE))      # Remove all variables, etc. from the session memory
+
+
+library(tibble)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(here)
+
+
+source("C:/Users/benjamin.levy/Desktop/Github/READ-PDB-blevy2-toy/TestScripts/Calc_strat_mean/fn_srs_survey_BENS.R")
+
+
+# 
+# # # # names of species files to read in  (GBhaddock)
+# # file.spp <- "ADIOS_SV_164744_GBK_NONE_survey_dist_map_fixed.csv"
+# 
+# 
+# # # names of species files to read in  (GByellowtail flounder)
+# file.spp <- "C:/Users/benjamin.levy/Desktop/NOAA/GIS_Stuff/From_Liz/survey_tow_latlon-20220107T192759Z-001/survey_tow_latlon/ADIOS_SV_172909_GBK_NONE_survey_dist_map_fixed.csv"
+# 
+# 
+# # # names of files to read in  (GBcod)
+# # file.spp <- "ADIOS_SV_164712_GBK_NONE_survey_dist_map_fixed.csv"
+# 
+# 
+# # # names of files to read in  (GOMcod)
+# # file.spp <- "ADIOS_SV_164712_GOM_NONE_survey_dist_map_fixed.csv"
+# 
+# 
+# # note: instead of manually pointing to individual input files ====
+# # can also use this code to get a list of all input files matching pattern, and then loop over those species
+# sp.list <- list.files(here("C:/Users/benjamin.levy/Desktop/NOAA/GIS_Stuff/From_Liz/survey_tow_latlon-20220107T192759Z-001/survey_tow_latlon/"), pattern="*_survey_dist_map_fixed.csv")
+# 
+
+
+# name of stratum area file  ====
+#file.sv.area <- "C:/Users/benjamin.levy/Desktop/NOAA/GIS_Stuff/From_Liz/strata_area.csv"
+
+
+# read in survey area ====
+#sv.area <- as_tibble(read.csv(here(file.sv.area) ) ) %>%
+ # select(-STRATUM_NAME)  #I think this is removing the column "stratum_name"
+
+#DEFINE INDIVIDUAL STRATUM AREAS IN SIMILAR MANNER AS ABOVE
+stratum <- c(1,2,3,4)
+STRATUM_AREA <- c(2500,2500,2500,2500) #100x100 grid so each corner has area 2500
+sv.area <- as_tibble(data.frame(stratum,STRATUM_AREA))
+
+# read in species file of tow by tow data from simulation
+spp <-  as_tibble(surv_random$log.mat,header=T)
+
+
+spp.name <- as.character("spp1")
+spp.stock <- as.character("Generic")
+#spp.itis <- unique(spp$SPECIES_ITIS)
+
+
+
+# get total area of stock ====
+spp.strata <- unique(spp$strata)
+spp.area <- sum(sv.area$STRATUM_AREA[sv.area$stratum %in% spp.strata]) #TOTAL AREA OF ALL STRATA
+
+
+# calculate SRS estimates ====
+spp.srs <- srs_survey(df=spp, sa=sv.area, str=NULL, ta=0.01, spp.name  )  # if strata=NULL, the function will use the unique strata set found in df
+                                                       # if strata!=NULL, the function will use that vector of strata
+spp.srs.rel.and.absolute <- spp.srs %>%
+  mutate(mean.yr.absolute=mean.yr*spp.area, sd.mean.yr.absolute=sd.mean.yr*spp.area,
+         CV.absolute=sd.mean.yr.absolute/mean.yr.absolute)
+
+write.csv(spp.srs.rel.and.absolute, file=paste("C:/Users/benjamin.levy/Desktop/NOAA/GIS_Stuff/From_Liz/",spp.name, spp.stock, "_SRS_from_Rcode.csv", sep="."), row.names=F)
+
+# Note: in spp.srs.rel.and.absolute, the relative mean index and sd are "mean.yr" and "sd.mean.yr"
+# the mean index scaled by total area ("absolute abundance index") are "mean.yr.absolute" and "sd.mean.yr.absolute"
---
+++ TestScripts/Calc_strat_mean/fn_srs_survey_BENS.R
@@ -0,0 +1,77 @@
+# function to calculate the SRS mean for NEFSC Survey
+# liz brooks  
+# 26 may 2021
+
+# edited by Ben Levy January 2022
+
+
+srs_survey <- function(df, sa, str, ta=0.01, spp.name = NULL  )  {
+  
+  # df is dataframe of survey data (tow by tow)  
+  # sa is vector of stratum area
+  # str is the matrix of strata info
+  # ta is the tow swept area (default 0.01 square nautical miles)
+
+
+  # add column to sa for number of tow units per stratum
+  sa <- sa %>%
+    mutate(STRATUM_UNITS = STRATUM_AREA/ta)
+  
+  if (is.null(str)) {  #grab unique strata by season
+    
+    strata <- df %>%
+      #filter(substr(SURVEY, 1, 4) =="NMFS") %>% #DONT THINK I NEED THIS
+      select(Season, stratum) %>%
+      group_by(Season) %>%
+      distinct(stratum) %>%
+      nest()
+    
+  } # end if check for null strata vector
+  
+ 
+  #make stratum both integers so they are same type in following block
+  sa$stratum <- as.integer(sa$stratum)
+  df$stratum <- as.integer(df$stratum)
+  
+  # calculate total area for stock-specific strata by season
+  tmp.total.area <- df %>%
+   # filter(substr(SURVEY, 1, 4) =="NMFS") %>%  #STILL DONT THINK I NEED THIS
+    select(Season, stratum) %>%
+    group_by(Season) %>%
+    distinct(stratum) %>% 
+    left_join(sa) %>%
+    summarise(Total=sum(STRATUM_AREA))
+  
+  #DAY COLUMN IN DF IS CAUSING ISSUES AND NOT NEEDED
+  df <- subset(df,select = -c(day))
+
+  tmp.tibble <- df %>%
+  #  filter(substr(SURVEY, 1, 4) =="NMFS") %>% #STILL DONT THINK I NEED THIS
+    group_by(Season) %>%
+    left_join(sa) %>%
+    replace(is.na(.), 0) %>%
+    pivot_longer(cols=c(spp.name), values_to="OBS_VALUE") %>%  #changing catchwt to obs_value
+    relocate(year, Season, stratum, tow, OBS_VALUE, STRATUM_AREA)
+
+# Calculate null survey
+surv.ind.str <- tmp.tibble %>%
+  select(year, Season, stratum,  STRATUM_AREA, tow, OBS_VALUE ) %>%
+  arrange(year, Season, stratum, tow) %>%
+  group_by(year, Season, stratum )  %>% #INDEX GOING DOWN TO STRATA
+  summarise(mean.str = mean(as.integer(OBS_VALUE)), var.samp.str=var(OBS_VALUE, na.rm=T), ntows.str = n() ) %>%
+  replace(is.na(.), 0) 
+
+surv.ind.yr <- surv.ind.str %>%
+  left_join(sa) %>%
+  left_join(tmp.total.area) %>%
+  mutate(mean.yr.str = (mean.str*STRATUM_AREA/Total), var.mean.yr.str=( (STRATUM_AREA^2/Total^2)*(1-ntows.str/STRATUM_UNITS)*(var.samp.str/ntows.str) ) )%>%
+  group_by(year, Season) %>% #INDEX GOING DOWN TO YEAR?
+  summarise(mean.yr = sum(mean.yr.str), var.mean.yr=sum(var.mean.yr.str)) %>% #SUMMARIZE TO CREAT NEW VARIABLES
+  mutate(sd.mean.yr=sqrt(var.mean.yr), CV=sd.mean.yr/mean.yr)  #MUTATE TO CREATE NEW VARIABLE FROM EXISTING VARIABLE
+
+
+return(surv.ind.yr)
+
+} # end function srs_nefsc
+
+
