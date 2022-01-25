376cc55b30c7dfefcd41ee905e4bfc6373da3ca3
Blevy2 <benjamin.levy@noaa.gov>
2022-01-25 09:06:33 -0500

    Made it work this time. Was only adding 2 weeks earlier

--- TestScripts/add_2_movecov_yrs.R
+++ TestScripts/add_2_movecov_yrs.R
@@ -1,8 +1,51 @@
 
+temp2 <- list(moveCov[["cov.matrix"]][[52]])
 
-temp<-list(moveCov[["cov.matrix"]][[1]],moveCov[["cov.matrix"]][[1]])
+for(i in seq(51,1)){
+  
+  
+  temp<-list(moveCov[["cov.matrix"]][[i]])
+
+  covmatrix <- list(moveCov[["cov.matrix"]][[i]])
+  
+  temp2 <- append(covmatrix,temp2)
+  
+  
+}
+ 
+
+#added two years to the front end that are copies of the first year
+append(temp2,moveCov[["cov.matrix"]]) -> new
+append(temp2,new) -> new
+
+# View(new)
+ 
+ moveCov[["cov.matrix"]]<-new
+
+ 
+ #remove extra stuff taking up memory
+ remove(temp)
+ remove(temp2)
+ remove(new)
+ remove(Move_Prob)
+ remove(Move_Prob_spwn)
+ remove(MoveProb)
+ remove(MoveProb_spwn)
+ 
+ remove(Good_moveCov)
+ remove(all_moveCov)
+ remove(B_moveCov)
+ remove(Pop)
+ remove(surv_random)
+ remove(fleets)
+ 
+ 
+ 
+ 
+ #old and wrong (only did 2 weeks)
+ temp<-list(moveCov[["cov.matrix"]][[1]],moveCov[["cov.matrix"]][[1]])
  append(temp,moveCov[["cov.matrix"]]) -> new
  View(new)
  moveCov[["cov.matrix"]]<-new
-
+ 
  remove(temp)
