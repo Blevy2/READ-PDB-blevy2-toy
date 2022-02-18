f7f9f00896553196a2899c731487124745d2895b
Blevy2 <benjamin.levy@noaa.gov>
2022-02-18 16:51:33 -0500

    working on habitat extraction

--- TestScripts/Environmental_Interp_Kern_Dens.R
+++ TestScripts/Environmental_Interp_Kern_Dens.R
@@ -190,7 +190,7 @@ plot_obs(test_lrren,
 # Trying to recreate above with my data
 ###########################################################
 
-loadedPackages <- c("envi", "raster", "RStoolbox", "spatstat.data", "spatstat.geom", "spatstat.core")
+loadedPackages <- c("rgdal", "data.table", "maptools","envi", "raster", "RStoolbox", "spatstat.data", "spatstat.geom", "spatstat.core")
 invisible(lapply(loadedPackages, library, character.only = TRUE))
 
 
@@ -239,22 +239,22 @@ plot(Sediment_poly)
 
 #ABOVE ARE POLYGONS. I CHANGED TO RASTERS IN ARCMAP AND LOAD THEM INSTEAD
 
-  #BATHYMETRY
-bathy_ras <- raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Bathy_raster\\Seafloor_Bathymetry_Clip1_Pr21.tif')
-plot(bathy_ras)
+  #BATHYMETRY   (OLD)
+#bathy_ras <- raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Bathy_raster\\Seafloor_Bathymetry_Clip1_Pr21.tif')
+#plot(bathy_ras)
 
-  #SEDIMENT
+  #SEDIMENT   (OLD)
 #categorical data (will this work?)
-sediment_ras_categ <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Atlantic_seafloor_sediment\\raster_categorical\\Sedim_ras_categ.tif')
-plot(sediment_ras_categ)
+#sediment_ras_categ <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Atlantic_seafloor_sediment\\raster_categorical\\Sedim_ras_categ.tif')
+#plot(sediment_ras_categ)
 
-#numerical data
-sediment_ras_num <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Atlantic_seafloor_sediment\\raster_number\\sediment_ras_num.tif')
-plot(sediment_ras_num)
+#numerical data   (OLD)
+#sediment_ras_num <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\Atlantic_seafloor_sediment\\raster_number\\sediment_ras_num.tif')
+#plot(sediment_ras_num)
 
-#SEDIMENT THICKNESS
-sediment_thick_ras <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\GlobalSedimentThickness\\Final_ras\\sed_thick_ras.tif')
-plot(sediment_thick_ras)
+#SEDIMENT THICKNESS   (OLD)
+#sediment_thick_ras <-  raster('C:\\Users\\benjamin.levy\\Desktop\\NOAA\\GIS_Stuff\\NOAA_Maps\\GlobalSedimentThickness\\Final_ras\\sed_thick_ras.tif')
+#plot(sediment_thick_ras)
 
 #Median sediment size (from Robyns USGS link)
 #extrapolated from points using natural neighbor interpolation method
@@ -266,27 +266,27 @@ plot(median_sed_thick_IDW)
 
 #DEPTH FROM FVCOM DATA
 depth_GB_ras <- readRDS(file="TestScripts/FVCOM_GB/depth_GB.RDS")
+plot(depth_GB_ras)
 
 
 
 
 
 
-
-
-#STOPPED HERE TRYING TO GET SEDIMENT AND DEPTH TO HAVE SAVE RESOLUTION
-
-
-#make resolutions match
+#make resolutions match (dont need this?)
 #extent(median_sed_thick_IDW) <- extent(depth_GB_ras)
-res(median_sed_thick_IDW) <- res(depth_GB_ras)
-depth_GB_ras@crs@projargs <- median_sed_thick_IDW@crs@projargs
+#test <- projectRaster(median_sed_thick_IDW,raster(GB_strata))
+#res(median_sed_thick_IDW) <- res(depth_GB_ras)
+#depth_GB_ras@crs@projargs <- median_sed_thick_IDW@crs@projargs
+
+
+
 
 #clip data to desired area using either
 #1) crop (for full extent. will create rectangle)
 #2) mask will cut to polygon
 
-bathy_ras <-crop(bathy_ras,GB_strata)
+#bathy_ras <-crop(bathy_ras,GB_strata)
 #sediment_ras_num <- crop(sediment_ras_num,GB_strata)
 #sedmient_ras_categ <- crop(sediment_ras_categ,GB_strata)
 #sediment_thick_ras <- crop(sediment_thick_ras,GB_strata)
@@ -294,14 +294,17 @@ median_sed_thick_NN <- raster::mask(median_sed_thick_NN,GB_strata_singlePoly)
 median_sed_thick_IDW <- raster::mask(median_sed_thick_IDW,GB_strata_singlePoly)
 depth_GB_ras <- raster::mask(depth_GB_ras,GB_strata_singlePoly)
 
-plot(bathy_ras)
-plot(GB_strata,add=T)
+#MAYBE DONT MASK TO AVOID ISSUES OF POINTS FALLING OUTSIDE RASTERS
 
-plot(sediment_ras_num)
-plot(GB_strata,add=T)
 
-plot(sediment_thick_ras)
-plot(GB_strata,add=T)
+# plot(bathy_ras)
+# plot(GB_strata,add=T)
+# 
+# plot(sediment_ras_num)
+# plot(GB_strata,add=T)
+# 
+# plot(sediment_thick_ras)
+# plot(GB_strata,add=T)
 
 plot(median_sed_thick_NN)
 plot(GB_strata,add=T)
@@ -309,35 +312,37 @@ plot(GB_strata,add=T)
 plot(median_sed_thick_IDW)
 plot(GB_strata,add=T)
 
-
+plot(depth_GB_ras)
+plot(GB_strata,add=T)
 
 #create image files to use later
-bathy_im<- as.im(bathy_ras)
-sediment_im <- as.im(sediment_ras_num) #CHANGE FROM CATEGORICAL TO NUMERICAL HERE
-sediment_thick_im <- as.im(sediment_thick_ras)
+#bathy_im<- as.im(bathy_ras)
+#sediment_im <- as.im(sediment_ras_num) #CHANGE FROM CATEGORICAL TO NUMERICAL HERE
+#sediment_thick_im <- as.im(sediment_thick_ras)
 median_sed_thick_IDW_im <- as.im(median_sed_thick_IDW)
 median_sed_thick_NN_im <- as.im(median_sed_thick_NN)
-
+depth_GB_im <- as.im(depth_GB_ras)
 
 
 #scale data
-bathy_im$v <- scale(bathy_im)
-sediment_im$v <- scale(sediment_im)
-sediment_thick_im$v <- scale(sediment_thick_im)
+#bathy_im$v <- scale(bathy_im)
+#sediment_im$v <- scale(sediment_im)
+#sediment_thick_im$v <- scale(sediment_thick_im)
 median_sed_thick_IDW_im$v <- scale(median_sed_thick_IDW_im)
 median_sed_thick_NN_im$v <- scale(median_sed_thick_NN_im)
+depth_GB_im$v <- scale(depth_GB_im)
 
 #old (wrong?) way
 #bathy_im_sc<- scale(as.im(bathy_ras))  #scaling here
 #sediment_im_sc <- scale(as.im(sediment_ras_num)) #CHANGE FROM CATEGORICAL TO NUMERICAL HERE
 
 #CHANGE BACK TO RASTER AFTER SCALING
-bathy_ras <- raster::raster(bathy_im)
-sediment_ras_num <- raster::raster(sediment_im)
-sediment_thick_ras <- raster::raster(sediment_thick_im)
+#bathy_ras <- raster::raster(bathy_im)
+#sediment_ras_num <- raster::raster(sediment_im)
+#sediment_thick_ras <- raster::raster(sediment_thick_im)
 median_sed_thick_IDW_ras <- raster::raster(median_sed_thick_IDW_im)
 median_sed_thick_NN_ras <- raster::raster(median_sed_thick_NN_im)
-
+depth_GB_ras <- raster::raster(depth_GB_im)
 
 
 
@@ -389,37 +394,91 @@ gis$Latitude <- gis$LATITUDE
 #replace NA with 0
 gis$CATCH_WT_CAL[is.na(gis$CATCH_WT_CAL)] <- 0
 
-spring.gis <- gis[gis$SEASON=='SPRING',]
-fall.gis   <- gis[gis$SEASON=='FALL',]
+#spring.gis <- gis[gis$SEASON=='SPRING',]
+#fall.gis   <- gis[gis$SEASON=='FALL',]
 
 
 #ONLY TAKE MORE RECENT ONES?
-spring.gis <- spring.gis[spring.gis$Year>2009,]
+#spring.gis <- spring.gis[spring.gis$Year>2009,]
+gis <- gis[gis$Year>=2009,]
+
 
 
 
 #setting up weighting following along from https://stackoverflow.com/questions/21273525/weight-equivalent-for-geom-density2d
+#this replaces each prescence point with CatchWt number of points
 library(data.table)
-weighted_data_spring <- data.table(spring.gis)[,list(Longitude=rep(Longitude,ceiling(CatchWt)),Latitude=rep(Latitude,ceiling(CatchWt)),CatchWt=rep(ceiling(CatchWt),ceiling(CatchWt)))]
+#weighted_data_spring <- data.table(spring.gis)[,list(Longitude=rep(Longitude,ceiling(CatchWt)),Latitude=rep(Latitude,ceiling(CatchWt)),CatchWt=rep(ceiling(CatchWt),ceiling(CatchWt)))]
+weighted_data_all <- data.table(gis)[,list(Longitude=rep(Longitude,ceiling(CatchWt)),Latitude=rep(Latitude,ceiling(CatchWt)),CatchWt=rep(ceiling(CatchWt),ceiling(CatchWt)))]
+
+
+
+
+
+
+# #removing points that fall outside one of our covariates
+# HAVENT QUITE FIGURED THIS OUT YET
+# # following from https://rspatial.org/raster/rosu/Chapter5.html
+# p <- weighted_data_all[,1:2]
+# win <- aggregate(GB_strata)
+# owin <- as.owin(sp::as_Spatial(win))
+# pp <- ppp(p[,1],p[,2],marks=, owin(c(-69.98, -65.68) ,c(40.09,42.45) ))
+# 
+# 
+# library(rgeos)
+# sp <- SpatialPoints(p,proj4string = CRS(proj4string((GB_strata))))
+# i <- gIntersects(sp,GB_strata_singlePoly,byid=TRUE)
+# which(!i)
+# 
+# 
+# for(x in length(presence)){
+#   for(y in length(presence$y)){
+#     
+#    if(median_sed_thick_IDW_im[x,y]){
+#      print("TRUE")
+#    } 
+#     
+#   }
+# }
+
+
+
+
+
+
+
 
 #convert data into planar point pattern (ppp). no weight option so use weighted/repeated points
-spring_points <- ppp(weighted_data_spring$Longitude,weighted_data_spring$Latitude,owin(c(-69.98, -65.68) ,c(40.09,42.45) ))
+#spring_points <- ppp(weighted_data_spring$Longitude,weighted_data_spring$Latitude,owin(c(-69.98, -65.68) ,c(40.09,42.45) ))
+
+#weighted points (2,181 presence and only 704 absence locations)
+all_points <- ppp(weighted_data_all$Longitude,weighted_data_all$Latitude,owin(c(-69.98, -65.68) ,c(40.09,42.45) ))
+
+#unweighted points (516 presence and 704 absence)
+temp <- data.table(gis)[CatchWt>0]
+all_points <- ppp(temp$Longitude,temp$Latitude,owin(c(-69.98, -65.68) ,c(40.09,42.45) ))
+
+
+
+
 
 
 
 #Add appropriate marks to the data from spatstat.data package. 
 #These points are considered our "presence" locations.
 
-presence <- spatstat.geom::unmark(spring_points)  #unmark removes existing attributes (aka marks)
+presence <- spatstat.geom::unmark(all_points)  #unmark removes existing attributes (aka marks)
 spatstat.geom::marks(presence) <- data.frame("presence" = rep(1, presence$n), #adds attributes back (aka marks)
                                              "lon" = presence$x,  #first repeat the nuimber 1 n times, then add x and y coordinates for each
                                              "lat" = presence$y)
 
 
-spatstat.geom::marks(presence)$bathy <-  bathy_im[presence] #adds covariate values for presence locations
+#spatstat.geom::marks(presence)$bathy <-  bathy_im[presence] #adds covariate values for presence locations
 #spatstat.geom::marks(presence)$sediment <- sediment_im[presence]
 #spatstat.geom::marks(presence)$sediment_thick <- sediment_thick_im[presence]
 spatstat.geom::marks(presence)$median_sed <- median_sed_thick_NN_im[presence]
+spatstat.geom::marks(presence)$median_sed <- median_sed_thick_IDW_im[presence]
+spatstat.geom::marks(presence)$depth <-  depth_GB_im[presence] #adds covariate values for presence locations
 
 
 
@@ -428,7 +487,7 @@ spatstat.geom::marks(presence)$median_sed <- median_sed_thick_NN_im[presence]
 
 #DEFINE ABSENCE LOCATIONS
 
-abs <-  data.table(spring.gis)[(CatchWt==0)]  #pull out absence points
+abs <-  data.table(gis)[(CatchWt==0)]  #pull out absence points
 
 #remove duplicate locations because it leads to problems later
 # NO IT DOESNT, THERE WERE POINTS OUTSIDE THE RASTER REGION
@@ -440,11 +499,14 @@ absence <-  ppp(abs$Longitude,abs$Latitude,owin(c(-69.98, -65.68) ,c(40.09,42.45
 spatstat.geom::marks(absence) <- data.frame("presence" = rep(0, absence$n),
                                             "lon" = absence$x,
                                             "lat" = absence$y)
-spatstat.geom::marks(absence)$bathy <-  bathy_im[absence] #some points are outside the region
+#spatstat.geom::marks(absence)$bathy <-  bathy_im[absence] #some points are outside the region
 #spatstat.geom::marks(absence)$ sediment <- sediment_im[absence]
 #spatstat.geom::marks(absence)$ sediment_thick <- sediment_thick_im[absence]
 
 spatstat.geom::marks(absence)$median_sed <- median_sed_thick_NN_im[absence]
+spatstat.geom::marks(absence)$median_sed <- median_sed_thick_IDW_im[absence]
+spatstat.geom::marks(absence)$depth <-  depth_GB_im[absence] #adds covariate values for presence locations
+
 
 
 
@@ -479,10 +541,13 @@ obs_locs <- obs_locs[ , c(6, 2, 3, 1, 4, 5)] #reorders columns so they are in co
 
 #Extract the prediction locations within the study area from one of the covariates.
 
-predict_locs <- data.frame(raster::rasterToPoints(bathy_ras))  #adds column called layer with bathymetry
+predict_locs <- data.frame(raster::rasterToPoints(depth_GB_ras))  #adds column called layer with depth
+
+#predict_locs <- data.frame(raster::rasterToPoints(bathy_ras))  #adds column called layer with bathymetry
 #predict_locs$layer2 <- raster::extract(sediment_ras_num, predict_locs[, 1:2]) #adds column called layer2 with sediment number
 #predict_locs$layer2 <- raster::extract(sediment_thick_ras, predict_locs[, 1:2]) #adds column called layer2 with sediment number
 predict_locs$layer2 <- raster::extract(median_sed_thick_NN_ras, predict_locs[, 1:2]) #adds column called layer2 with sediment number
+predict_locs$layer2 <- raster::extract(median_sed_thick_IDW_ras, predict_locs[, 1:2]) #adds column called layer2 with sediment number
 
 
 
@@ -498,7 +563,9 @@ fish_lrren <- lrren(obs_locs = obs_locs,
                     predict_locs = predict_locs,
                     predict = TRUE,
                     cv = TRUE,
+                   # adapt=T,
                     #balance = TRUE)
+                    #conserve = TRUE #Logical. If TRUE (the default), the ecological niche will be estimated within a concave hull around the locations in obs_locs. If FALSE, the ecological niche will be estimated within a concave hull around the locations in predict_locs.
 )
 end_time <- Sys.time() # record end time
 lrren_time <- end_time - start_time # calculate duration of lrren() example
@@ -528,6 +595,26 @@ plot_predict(fish_lrren, cref0 = "EPSG:32632", cref1 = "EPSG:4326",
 
 
 
+
+
+
+
+
+#Trying my own lrren function that uses adapt=T option
+source("TestScripts/lrren_BENS.R")
+
+fish_lrren <- lrren_Bens(obs_locs = obs_locs,
+                    predict_locs = predict_locs,
+                    predict = TRUE,
+                    cv = FALSE, #if true get error foreach::foreach comb not defined
+                    adapt=T,
+                    #balance = TRUE)
+                    #conserve = TRUE #Logical. If TRUE (the default), the ecological niche will be estimated within a concave hull around the locations in obs_locs. If FALSE, the ecological niche will be estimated within a concave hull around the locations in predict_locs.
+)
+
+
+
+
 #to source a plot_predict where I save output
 #1: load file
 source("TestScripts/plot_predict_BENS.R") #my edited version that saves output
@@ -541,20 +628,27 @@ p<-plot_predict_BENS(fish_lrren, cref0 = "EPSG:32632", cref1 = "EPSG:4326",
              lower_lrr = -1, #used to be -1 to 1
              upper_lrr = 1)
 
-
+#choose one. both from plot_predict_BENS
+p$out<-predict_risk_raster
+p$out<-rrp$v
 #rescale output from -1 to 1 to 0 to 1
 
 #first exponentiate log values
-vec <- exp(p$out$v@data@values)
+vec <- exp(p$out@data@values)
 range(vec,na.rm=T)
+
+#maybe converts to correct interval?
+#check https://www.statisticshowto.com/log-odds/
+#vec <- vec/(1+vec)
 
 #then convert to 0 to 1 range
 vec_01 <-(vec - min(vec,na.rm=T)) / (max(vec,na.rm=T) - min(vec,na.rm=T)) 
 range(vec_01,na.rm=T)
 
+
 p$out$v@data@values <- vec_01
 
-
+#MUCH OF THIS CODE IS FROM ENVI::DIV_PLOT
 #rescale legend labels and colors
 
 #scale to display
@@ -565,8 +659,14 @@ rbs <- seq( min(vec_01,na.rm=T),  max(vec_01,na.rm=T), rbt)
 #numbers to show
 rbl <- round(rbs, digits = 1)
 
+
+midpoint<-0
+lowerhalf <- length(out[out < midpoint & !is.na(out)]) # values below 0
+upperhalf <- length(out[out > midpoint & !is.na(out)]) # values above 0
+
 # Color ramp parameters
 ## Colors
+cols = c("#8B3A3A", "#CCCCCC", "#0000CD")
 ### vector of colors for values below midpoint
 rc1 <- grDevices::colorRampPalette(colors = c(cols[3], cols[2]), space = "Lab")(lowerhalf)
 ### vector of colors for values above midpoint
@@ -577,7 +677,24 @@ rampcols <- c(rc1, rc2)
 
 
 graphics::par(pty = "s")
+
+#fix extent
+p$out$v@extent <- GB_strata_ras@extent
+
 fields::image.plot(p$out$v, breaks = p$out$breaks, col = p$out$cols, 
                    axes = TRUE, main = "log relative risk", xlab = "longitude", 
                    ylab = "latitude", legend.mar = 3.1, axis.args = list(at = rbs, 
                                                                          las = 0, labels = rbl, cex.axis = 0.67))
+
+
+
+
+final_ras <- raster::mask(p$out$v,GB_strata_singlePoly)
+
+final_matrix <- as.matrix(final_ras)
+
+#turn matrix for plotting
+rotate <- function(x) t(apply(x, 2, rev))
+final_matrix_turned <- rotate(final_matrix)
+par(mar=c(1,1,1,1))
+fields::image.plot(final_matrix_turned)
